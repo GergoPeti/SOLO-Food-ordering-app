38a2836ff8d283e508a5818c07ee5ee21e79bd9b
Gergő Peti <96701860+GergoPeti@users.noreply.github.com>
2023-04-07 21:29:58 +0200

    Bug in the pay button

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 
 import { menuArray } from './data.js';
 
-// Selecting elements
-const addBtns = document.querySelectorAll('.add-btn');
-
 // Loading the content with JS
 function loadMenuContent() {
   let menuHTML = '';
@@ -36,31 +33,69 @@ function renderMenu() {
 
 renderMenu();
 
-// when click on the '+' add food object in a new array called 'order'
-// render on the page with JS
-// make 'remove' button work, when pushed delete object from array
-// render this change on the page with JS
-// SUM the ordered food price
-
 const orders = [];
-// 1 eventListener on the page
+
 document.addEventListener('click', function (e) {
-  // console.log(e.target.parentElement.parentElement);
-  // console.log(e.target.dataset.uuid);
-
-  orders.push(
-    menuArray.filter(menuObj => menuObj.id === Number(e.target.dataset.uuid))[0]
-  );
-
-  console.log('orderLIST', orders);
+  if (e.target.dataset.uuid) {
+    orders.push(
+      menuArray.filter(
+        menuObj => menuObj.id === Number(e.target.dataset.uuid)
+      )[0]
+    );
+  } else if (e.target.dataset.remove) {
+    orders.splice(
+      orders.indexOf(
+        menuArray.filter(
+          menuObj => menuObj.id === Number(e.target.dataset.remove)
+        )[0]
+      ),
+      1
+    );
+  }
 
   renderOrders();
 });
 
-function renderOrders() {
-  console.log(document.querySelector('.order-sum'));
+renderOrders();
 
-  document.querySelector('.order-sum').innerHTML = loadOrderContent();
+document.addEventListener('click', function (e) {
+  if (e.target.className === 'complete-btn') {
+    document.querySelector('.form-box_container').classList.remove('hidden');
+  } else if (e.target.className === 'esc') {
+    document.querySelector('.form-box_container').classList.add('hidden');
+  } // else if (e.target.id === 'form-btn') {
+  // console.log('form-btn clicked');
+  // document.querySelector('.order-bigbox').classList.add('hidden');
+  // document.querySelector('.form-box_container').classList.add('hidden');
+  //}
+  console.log(e);
+});
+
+const payBtn = document.querySelector('#form-btn');
+
+payBtn.addEventListener('click', function (e) {
+  // e.preventDefault();
+  console.log('form-btn clicked');
+  document.querySelector('.order-bigbox').classList.add('hidden');
+  document.querySelector('.form-box_container').classList.add('hidden');
+});
+
+// Closing payment container with Escape key
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    document.querySelector('.form-box_container').classList.add('hidden');
+  }
+});
+
+function renderOrders() {
+  document.querySelector('.order-bigbox').classList.remove('hidden');
+  document.querySelector('.insert-orders').innerHTML = loadOrderContent();
+
+  // if (orders.length === 0) {
+  //   document.querySelector('.insert-orders').classList.add('hidden');
+  // }
+
+  totalPriceCalc();
 }
 
 function loadOrderContent() {
@@ -70,11 +105,19 @@ function loadOrderContent() {
     orderItem =>
       (orderHTML += `<div class="order-item">
     <p class="food-name">${orderItem.name}</p>
-    <p class="remove-order">remove</p>
+    <p class="remove-order" data-remove="${orderItem.id}">remove</p>
     <p class="food-price marle-auto">$${orderItem.price}</p>
-  </div>
-  `)
+  </div>`)
   );
 
   return orderHTML;
+}
+
+function totalPriceCalc() {
+  let price = 0;
+  orders.forEach(order => (price += order.price));
+
+  document.querySelector('.sum-price_box').innerHTML = `<p>Total price:</p>
+  <p>$${price}</p>
+  `;
 }
