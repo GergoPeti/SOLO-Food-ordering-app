ff0e61d224370d4d38e845e7e5d235d1c682cc86
Gergő Peti <96701860+GergoPeti@users.noreply.github.com>
2023-04-07 14:25:48 +0200

    rendering orders with renderOrders function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ import { menuArray } from './data.js';
 const addBtns = document.querySelectorAll('.add-btn');
 
 // Loading the content with JS
-const loadMenuContent = function () {
+function loadMenuContent() {
   let menuHTML = '';
 
   menuArray.forEach(
@@ -21,17 +21,60 @@ const loadMenuContent = function () {
       <p class="food-price">$${menu.price}</p>
     </div>
     <div class="add-btn_box">
-      <div class="add-btn">+</div>
+      <div class="add-btn" data-uuid="${menu.id}">+</div>
     </div>
   </div>`)
   );
 
   return menuHTML;
-};
+}
 
 // Rendering the loaded content
-const renderMenu = function () {
+function renderMenu() {
   document.querySelector('.menu').innerHTML = loadMenuContent();
-};
+}
 
 renderMenu();
+
+// when click on the '+' add food object in a new array called 'order'
+// render on the page with JS
+// make 'remove' button work, when pushed delete object from array
+// render this change on the page with JS
+// SUM the ordered food price
+
+const orders = [];
+// 1 eventListener on the page
+document.addEventListener('click', function (e) {
+  // console.log(e.target.parentElement.parentElement);
+  // console.log(e.target.dataset.uuid);
+
+  orders.push(
+    menuArray.filter(menuObj => menuObj.id === Number(e.target.dataset.uuid))[0]
+  );
+
+  console.log('orderLIST', orders);
+
+  renderOrders();
+});
+
+function renderOrders() {
+  console.log(document.querySelector('.order-sum'));
+
+  document.querySelector('.order-sum').innerHTML = loadOrderContent();
+}
+
+function loadOrderContent() {
+  let orderHTML = '';
+
+  orders.forEach(
+    orderItem =>
+      (orderHTML += `<div class="order-item">
+    <p class="food-name">${orderItem.name}</p>
+    <p class="remove-order">remove</p>
+    <p class="food-price marle-auto">$${orderItem.price}</p>
+  </div>
+  `)
+  );
+
+  return orderHTML;
+}
